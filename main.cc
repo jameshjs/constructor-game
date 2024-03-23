62779f909b127f908858819766821a9dcfc66e41
MrShamie <ablitzcrank@gmail.com>
2024-03-23 19:16:47 -0400

    read and print board

--- main.cc
+++ main.cc
@@ -1,4 +1,12 @@
-//asdf
-int main(){
-    return 0; // test
+#include "board.h"
+#include "readboard.h"
+#include "textdisplay.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	Board board = ReadBoard().create();
+	TextDisplay td{&board};
+	td.print(cout);
+	return 0;
 }
