b5dea50e19b57630450d08046a9ce1a74b4e7562
MrShamie <ablitzcrank@gmail.com>
2024-03-31 21:51:32 -0400

    Dice roll is now functional

--- game.cc
+++ game.cc
@@ -1,7 +1,7 @@
 #include "game.h"
 using namespace std;
 
-Game::Game() : board{ReadBoard().create()}, td{TextDisplay{&board}}, current_player{Colour::Blue} {
+Game::Game() : board{ReadBoard().create()}, td{TextDisplay{&board}}, current_player{Colour::Blue}, round_number(0) {
 	players.insert(make_pair(Colour::Blue, Player(Colour::Blue)));
 	players.insert(make_pair(Colour::Red, Player(Colour::Red)));
 	players.insert(make_pair(Colour::Orange, Player(Colour::Orange)));
