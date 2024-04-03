28b47234e29527ce19a53a70b54a1f3645fb4e60
MrShamie <ablitzcrank@gmail.com>
2024-04-03 13:18:13 -0400

    minor fix

--- constructor-game-bonus/random.h
+++ constructor-game-bonus/random.h
@@ -1,19 +1,3 @@
-// Pseudo Random Number Generator : generate repeatable sequence of values that
-//   appear random by scrambling the bits of a 32-bit integer value.
-//
-// Interface :
-//   PRNG(s) - set starting seed 
-//   seed() - read seed
-//   seed(s) - reset seed
-//   prng() - generate random value in range [0,UINT_MAX]
-//   prng(u) - generate random value in range [0,u]
-//   prng(l,u) - generate random value in range [l,u]
-//
-// Examples : generate random number between 5-21
-//   prng() % 17 + 5	values 0-16 + 5 = 5-21
-//   prng( 16 ) + 5
-//   prng( 5, 21 )
-
 #ifndef __RNG_H__
 #define __RNG_H__
 
