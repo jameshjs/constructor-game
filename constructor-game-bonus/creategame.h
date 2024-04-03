997c4f8573bd3f4524f7e067fc6e4d8ab2aca9a8
MrShamie <ablitzcrank@gmail.com>
2024-04-03 00:14:41 -0400

    testing

--- constructor-game-bonus/creategame.h
+++ constructor-game-bonus/creategame.h
@@ -18,6 +18,7 @@ class CreateGame : public Factory {
 		uint32_t seed;
 
 		Board create_board();
+
 	public:
 		CreateGame(const std::string filename = "layout.txt", LoadType type = LoadType::Board, uint32_t seed = 362436069);
 		Game create() override;
